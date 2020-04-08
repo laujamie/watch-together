f26468c5cb70a19799c8fa7db98daae186204884
laujamie <36782341+laujamie@users.noreply.github.com>
2020-04-08 02:59:37 +0000

    Switch back to JS

--- client/src/styles/YoutubePlayerContainerStyles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const YoutubePlayerContainerWrapper = styled.div`
-  overflow: hidden;
-  padding-top: 56.25%;
-  position: relative;
-  height: 0;
-`;
--- client/src/styles/YoutubePlayerStyles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const YoutubePlayerWrapper = styled.iframe`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
