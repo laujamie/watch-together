2374bd5d32637c8f8e26e8ad20cccd3891259ea9
laujamie <36782341+laujamie@users.noreply.github.com>
2020-04-19 16:52:09 +0000

    Update server formatting

--- server/server.js
+++ server/server.js
@@ -1,32 +1,36 @@
-const env = require("dotenv").config();
-const express = require("express");
-const http = require("http");
-const socketIO = require("socket.io");
-const cors = require("cors");
+const env = require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const indexRouter = require("./routes/index");
-
 const corsOptions = { origin: process.env.CORS_DOMAIN };
 
 app.use(cors(corsOptions));
 
-app.use(express.static("../client/build"));
+app.use(express.static('../client/build'));
 
-io.on("connection", (socket) => {
-  socket.on("pause", () => {
-    socket.broadcast.emit("pause");
+io.on('connection', (socket) => {
+  console.log('connected');
+  socket.on('pause', () => {
+    socket.broadcast.emit('pause');
+    console.log('paused');
   });
-  socket.on("play", () => {
-    socket.broadcast.emit("play");
-    console.log("played");
+  socket.on('play', () => {
+    socket.broadcast.emit('play');
+    console.log('played');
   });
-  socket.on("video id", (videoId) => {
-    socket.broadcast.emit("video id", videoId);
+  socket.on('video id', (videoId) => {
+    socket.broadcast.emit('video id', videoId);
+  });
+  socket.on('timestamp', (timestamp) => {
+    console.log(timestamp);
+    io.emit('timestamp', timestamp);
   });
 });
 
